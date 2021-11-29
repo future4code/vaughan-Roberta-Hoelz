791f401f2b9f4275a54a497f984a0d4a4da730b7
Roberta Rocha Hoelz <robertahoelz@hotmail.com>
2021-11-29 16:56:25 -0300

    hoje estou testanto o JavaScript

---
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,16 @@
+/* Resposta 1: 10 10 5
+Resposta 2: 20 20 20
+Resposta 3: let horasDia = prompt("Quantas horas você trabalha por dia?");
+let salarioDia = prompt("Quanto você recebe por dia?")
+alert(`Você recebe ${salarioDia/horasDia} por hora`)
+
+------------------------------------------------------------
+const nomeCompleto = prompt("Digite aqui seu nome completo")
+let idade = prompt("Digite aqui sua idade")
+
+typeof nomeCompleto
+typeof idade
+
+console.log("Hey","Me chamo",nomeCompleto,"minha idade é",idade,"anos")
+*/
+
