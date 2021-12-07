36793f3d5242d248bee4bfa462fd8eb40364d1db
Roberta Rocha Hoelz <robertahoelz@hotmail.com>
2021-12-06 23:53:43 -0300

    primeiros exercícios de objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,9 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+//QUESTÃO 1 - Matheus Nachtergaele, Virginia Cavendish, Globo, 14h
+
+//QUESTÃO 2 
+  //LETRA A) JUCA
+           //JUBA
+           //JUBO
+  //LETRA B) 
