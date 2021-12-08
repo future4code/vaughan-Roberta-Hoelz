c16fac3c8abc3059507fa5f75a511cb7fcc11e2c
Roberta Rocha Hoelz <robertahoelz@hotmail.com>
2021-12-07 23:58:20 -0300

    parcial condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,5 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO DE CODIGO
+//QUESTÃO 1
+//LETRA A) Ele testa se o número inserido pelo usuário deixa como resto da divisão o valor igual a zero.
+//LETRA B) Para todos os números pares
+//LETRA C) Para todos os números ímpares
