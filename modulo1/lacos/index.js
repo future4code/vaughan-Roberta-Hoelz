7ea857a0eeef5b98fa1e0fa1206422571770d770
Roberta Rocha Hoelz <robertahoelz@hotmail.com>
2021-12-08 23:23:43 -0300

    Parcial da aula de lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,4 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGOS
+//QUESTÃO 1 - O código está criando um looping, que se inicia no número zero e soma 1 ao valor até que chegue a um numero inteiro menor do que 5.
+// Será impresso no console: 0, 1, 2, 3, 4
+
