5b0312700dc7f2e6940eb5689e8a65ba285db6e7
Roberta Rocha Hoelz <robertahoelz@hotmail.com>
2021-12-09 21:29:10 -0300

    Parte do exercício de callbacks

---
+++ modulo1/callbacks/index.js
@@ -0,0 +1,12 @@
+// EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGOS
+// QUESTÃO 1
+// Será impressa uma lista com os nomes e apelidos dos usuários.
+
+// QUESTÃO 2
+// Será impressa uma lista somente com os nomes dos usuários.
+
+// QUESTÃO 3
+// Será impressa uma lsita somente dos apelidos dos usuários.
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+// QUESTÃO 1
